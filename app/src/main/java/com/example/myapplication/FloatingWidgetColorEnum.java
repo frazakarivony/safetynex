96f9eda97d64b02bf1134da0ee5e096d2d5b0442
Frederic Razakarivony <frazakarivony@2bconsulting.com>
2019-08-26 10:09:43 +0200

    [FRA] gestion répétition d'alerte

---
+++ app/src/main/java/com/example/myapplication/FloatingWidgetColorEnum.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+public enum FloatingWidgetColorEnum {
+    //Objets directement construits
+    LOW_OF_LOWLEVEL("LOW", "BLACK"),
+    MEDIUM_OF_LOWLEVEL("MEDIUM", "BLACK"),
+    HIGH_OF_LOWLEVEL("HIGH", "BLACK"),
+    WARNING("WARNING", "BLACK"),
+    WARNING_SPEED("WARNING_SPEED", "ORANGE"),
+    GPS_LOST("GPS_LOST", "GREY"),
+    ALERT("ALERT", "RED");
+
+    private String floatingWidgetBorderColor = "";
+    private String floatingWidgetTxtColor = "";
+
+    //Constructeurs
+    FloatingWidgetColorEnum(String floatingWidgetBorderColor, String floatingWidgetTxtColor){
+        this.floatingWidgetBorderColor = floatingWidgetBorderColor;
+        this.floatingWidgetTxtColor = floatingWidgetTxtColor;
+    }
+
+    public String getFloatingWidgetBorderColor(){
+        return floatingWidgetBorderColor;
+    }
+
+    public String getFloatingWidgetTxtColor(){
+        return floatingWidgetTxtColor;
+    }
+
+}
